6b7df53da324b3f1e573c164569a560c601a4749
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2021-04-24 22:39:00 +0900

    Layoutを追加

---
+++ components/Layout.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react'
+import Head from 'next/head'
+
+type Props = {
+  children: ReactNode
+  title?: string
+}
+
+const Layout: React.FC<Props> = ({ children, title = "Nextjs Shopify Store" }) => (
+  <>
+    <Head>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    {children}
+  </>
+);
+
+export default Layout
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
-const home = () => (
-  <div>hello</div>
-)
+import React from 'react'
+import Layout from "../components/Layout"
+
+const home: React.FC = () => (
+  <Layout>
+    <div>ただのトップページです。</div>
+  </Layout>
+);
 
 export default home;
