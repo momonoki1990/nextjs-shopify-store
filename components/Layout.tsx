34cb201c98b398e1256b3c762ede095ede2365f9
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2021-04-25 22:02:13 +0900

    絶対パス指定を可能に

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react'
 import Head from 'next/head'
-import Header from './Header'
-import Footer from './Footer'
+import Header from 'components/Header'
+import Footer from "components/Footer";
 
 type Props = {
   children: ReactNode
