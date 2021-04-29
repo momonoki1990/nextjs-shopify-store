3068d49fbe37438c91b9dc9564a75f34734cc85e
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2021-04-29 18:00:04 +0900

    ページネーションボタンを若干の修正

--- components/utils/Pagination.tsx
+++ components/utils/Pagination.tsx
@@ -1,31 +1,31 @@
 import React from "react";
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 type Props = {
-  currentPage: number
-  totalPage: number
-}
+  currentPage: number;
+  totalPage: number;
+};
 
 const Paginateion: React.FC<Props> = ({ currentPage, totalPage }) => {
-  const previousPage = currentPage - 1;
-  const nextPage = currentPage + 1;
   const router = useRouter();
+  const previousPage: string = `${router.pathname}/?page=${currentPage - 1}`;
+  const nextPage: string = `${router.pathname}/?page=${currentPage + 1}`;
 
   return (
     <div>
       {currentPage === 1 ? (
         <span style={{ color: "gray" }}>←</span>
       ) : (
-        <Link href={`${router.pathname}?page=${previousPage}`}>
+        <Link href={previousPage}>
           <a>←</a>
         </Link>
-      )}
-      {currentPage} / {totalPage}
+      )}{" "}
+      ページ: {currentPage} / {totalPage}{" "}
       {currentPage === totalPage ? (
         <span style={{ color: "gray" }}>→</span>
       ) : (
-        <Link href={`${router.pathname}?page=${nextPage}`}>
+        <Link href={nextPage}>
           <a>→</a>
         </Link>
       )}
