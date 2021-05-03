b50c66dc2153ca6606f6a49e64f92c307dc6b35d
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2021-05-04 06:21:28 +0900

    ヘッダーアイコン途中

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 const Header: React.FC = () => (
   <header className="header border-b border-gray--600">
-    <div className="header__inner container items-center grid grid-cols-2 md:grid-cols-3 h-14 md:h-20 xl:max-w-none xl:px-14">
-      <div className="header__logo text-left">
+    <div className="header__inner container items-center flex justify-between md:grid md:grid-cols-3 xl:max-w-none xl:px-14">
+      <div className="header__logo text-left mt-6 mb-6">
         <Link href="/">
           <a className="font-semibold text-xl text-gray-800 hover:text-opacity-70 tracking-widest">
             SAMPLE-KUMA-STORE1
@@ -31,8 +31,8 @@ const Header: React.FC = () => (
         </ul>
       </nav>
 
-      <div className="header__icons text-right">
-        <span className="search inline-block text-gray-700">
+      <div className="header__icons flex items-center justify-end">
+        <button className="search inline-block px-2 text-gray-700">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6"
@@ -47,8 +47,26 @@ const Header: React.FC = () => (
               d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
             />
           </svg>
-        </span>
-        <span className="bag inline-block text-gray-700">
+        </button>
+        <Link href="/cart">
+          <a className="bag inline-block px-2 text-gray-700">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+              />
+            </svg>
+          </a>
+        </Link>
+        <button className="ban inline-block md:hidden pl-2 text-gray-700">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6"
@@ -60,10 +78,10 @@ const Header: React.FC = () => (
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth={2}
-              d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+              d="M4 6h16M4 12h16M4 18h16"
             />
           </svg>
-        </span>
+        </button>
       </div>
     </div>
   </header>
