94015216a58817cc31f1d725782accaa8f2f8936
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2021-05-11 20:37:29 +0900

    Revert "ProductImage.tsxに画面幅による分岐を追加"
    This reverts commit b1a6abba1134ece6104473f67ac82ddc8e85e8c9.

--- components/product/ProductImageList.tsx
+++ components/product/ProductImageList.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/image";
 import { Product } from "shopify-buy";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { Swiper, SwiperSlide } from "swiper/react";
 // import Swiper core and required modules
 import SwiperCore, { Navigation } from "swiper/core";
@@ -20,56 +19,52 @@ const ProductImageList: React.FC<Props> = ({
   currentImageId,
   setImageId,
 }) => {
-  const isOverMd = useMediaQuery("(min-width:768px)", { noSsr: true });
-  
   return (
     <>
-      {isOverMd ? (
-        <div className="grid grid-cols-3 lg:grid-cols-4">
+      <div className="hidden md:grid md:grid-cols-3 lg:grid-cols-4">
+        {product.images.map((image) => {
+          const imageId = image.id as string;
+          const isCurrentImage = imageId === currentImageId;
+          const borderClass = " " + "border-2 border-gray-800";
+          return (
+            <figure
+              className={"m-0" + (isCurrentImage ? borderClass : "")}
+              key={image.id}
+            >
+              <a onClick={() => setImageId(imageId)}>
+                <Image priority src={image.src} height={400} width={400} />
+              </a>
+            </figure>
+          );
+        })}
+      </div>
+
+      <div className="md:hidden">
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          onSwiper={(swiper) => console.log(swiper)}
+          navigation={true}
+        >
           {product.images.map((image) => {
             const imageId = image.id as string;
             const isCurrentImage = imageId === currentImageId;
             const borderClass = " " + "border-2 border-gray-800";
             return (
-              <figure
-                className={"m-0" + (isCurrentImage ? borderClass : "")}
-                key={image.id}
-              >
-                <a onClick={() => setImageId(imageId)}>
-                  <Image priority src={image.src} height={400} width={400} />
-                </a>
-              </figure>
+              <SwiperSlide key={image.id}>
+                <figure
+                  className={"m-0" + (isCurrentImage ? borderClass : "")}
+                  key={image.id}
+                >
+                  <a onClick={() => setImageId(imageId)}>
+                    <Image priority src={image.src} height={400} width={400} />
+                  </a>
+                </figure>
+              </SwiperSlide>
             );
           })}
-        </div>
-      ) : (
-        <div>
-          <Swiper spaceBetween={50} slidesPerView={3} navigation={true}>
-            {product.images.map((image) => {
-              const imageId = image.id as string;
-              const isCurrentImage = imageId === currentImageId;
-              const borderClass = " " + "border-2 border-gray-800";
-              return (
-                <SwiperSlide key={image.id}>
-                  <figure
-                    className={"m-0" + (isCurrentImage ? borderClass : "")}
-                    key={image.id}
-                  >
-                    <a onClick={() => setImageId(imageId)}>
-                      <Image
-                        priority
-                        src={image.src}
-                        height={400}
-                        width={400}
-                      />
-                    </a>
-                  </figure>
-                </SwiperSlide>
-              );
-            })}
-          </Swiper>
-        </div>
-      )}
+        </Swiper>
+      </div>
     </>
   );
 };
