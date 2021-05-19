1a1e44800a9c065efd648c0c59b80b1b5ef778e6
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2021-05-20 06:50:17 +0900

    環境変数周りを修正(publicに)

--- lib/client.ts
+++ lib/client.ts
@@ -4,13 +4,13 @@ import UnoptimizedClient from 'shopify-buy/index.unoptimized.umd';
 
 
 const client = Client.buildClient({
-  domain: process.env.DOMAIN,
-  storefrontAccessToken: process.env.SHOPIFYTOKEN,
+  domain: process.env.NEXT_PUBLIC_DOMAIN,
+  storefrontAccessToken: process.env.NEXT_PUBLIC_SHOPIFYTOKEN,
 });
 
 export const unoptimizedclient = UnoptimizedClient.buildClient({
-  domain: process.env.DOMAIN,
-  storefrontAccessToken: process.env.SHOPIFYTOKEN,
+  domain: process.env.NEXT_PUBLIC_DOMAIN,
+  storefrontAccessToken: process.env.NEXT_PUBLIC_SHOPIFYTOKEN,
 });
 
 export default client
