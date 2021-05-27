abcaccec6139fd0eef9b11a2c23ee777c4bde9ec
kumasegawa <n.kumasegawa@kol-tech.co.jp>
2021-05-28 06:50:03 +0900

    /collections/allに商品ページへのリンクを設定

--- components/collections/ProductCard.tsx
+++ components/collections/ProductCard.tsx
@@ -1,25 +1,29 @@
 import React from "react";
 import Image from "next/image";
-import { Product } from "shopify-buy";
+import Link from "next/link";
+// import { Product } from "shopify-buy";
 import getPriceInfoFromProduct from "lib/getPriceInfoFromProduct"
 
 type Props = {
-  product: Product;
+  product: any;
 };
 
 const ProductCard: React.FC<Props> = ({ product }) => {
   const { price, priceVaries } = getPriceInfoFromProduct(product);
+  console.log(JSON.stringify(product.handle))
 
   return (
     <div className="product-card">
       <div className="product-card__inner">
         <figure className="product-card__image" style={{ margin: 0 }}>
-          <Image
-            priority
-            src={product.images[0].src}
-            height={400}
-            width={400}
-          />
+          <Link href={`/products/${product.handle}`}>
+            <Image
+              priority
+              src={product.images[0].src}
+              height={400}
+              width={400}
+            />
+          </Link>
         </figure>
         <div className="product-card__info mt-2" style={{ textAlign: "left" }}>
           <div className="product-card__title font-semibold text-sm md:text-base">
