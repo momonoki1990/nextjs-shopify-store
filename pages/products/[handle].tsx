b4eb7958e016cec2bc61e781965e1f5b44c71ab7
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2021-05-31 06:50:30 +0900

    クエリパラメータをサーバー側で取得

--- pages/products/[handle].tsx
+++ pages/products/[handle].tsx
@@ -1,16 +1,16 @@
 import React, { useState, createContext } from "react";
 import { GetServerSideProps } from "next";
-import { useRouter } from "next/router";
 import { Product, ProductVariant } from "shopify-buy";
 import client from "lib/client";
 import Layout from "components/common/Layout";
 import ProductImage from "components/product/ProductImage";
 import ProductDetail from "components/product/ProductDetail";
-import ProductCard from "components/collections/ProductCard";
+
+
 
 type Props = {
   product: any;
-  variant: any | null;
+  variantId: string | null;
 };
 
 type ProductContext = {
@@ -23,24 +23,21 @@ type ProductContext = {
 
 export const ProductContext = createContext({} as ProductContext);
 
-const ProductPage: React.FC<Props> = ({ product }) => {
 
-  // query paramsのvariantを元にvariantオブジェクトを取得
-  const router = useRouter();
-  const variantId = router.query.variant;
-  
+
+const ProductPage: React.FC<Props> = ({ product, variantId }) => {
+
   const [variant, setVariant] = useState<ProductVariant | null>(variantId ? product.variants.find((v) => v.id === variantId) : null)
-
   const [imageId, setImageId] = useState<string>(
     (variant?.image?.id || product.images[0].id) as string
   );
 
   const ProductContextValue: ProductContext = {
-    product: product,
-    variant: variant,
-    setVariant: setVariant,
-    imageId: imageId,
-    setImageId: setImageId,
+    product,
+    variant,
+    setVariant,
+    imageId,
+    setImageId,
   };
 
   return (
@@ -49,10 +46,10 @@ const ProductPage: React.FC<Props> = ({ product }) => {
         <article className="product">
           <section className="container md:grid md:grid-cols-2 md:gap-x-8">
             <div className="product__image mb-12 md:mb-0">
-              <ProductImage/>
+              <ProductImage />
             </div>
             <div className="product__detail">
-              <ProductDetail/>
+              <ProductDetail />
             </div>
           </section>
         </article>
@@ -61,14 +58,17 @@ const ProductPage: React.FC<Props> = ({ product }) => {
   );
 };
 
+
+
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const handle = context.params.handle as string;
   const product: Product = await client.product.fetchByHandle(handle);
-  const variantId = context.query?.variant;
-  
+  const variantId: string | null = context.query.variant as string || null;
+
   return {
     props: {
       product: JSON.parse(JSON.stringify(product)),
+      variantId
     },
   };
 };
