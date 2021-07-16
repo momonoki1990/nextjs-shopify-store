9dd216cbc874d8243fe1b992f7dc5e241e60f111
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2021-07-16 20:28:40 +0900

    コレクションのリンクを追加

--- components/common/Header/MenuList.tsx
+++ components/common/Header/MenuList.tsx
@@ -1,8 +1,8 @@
-import React from "react"
+import React from "react";
 
 type Props = {
-  visibility: string
-}
+  visibility: string;
+};
 
 const MenuList: React.FC<Props> = ({ visibility }) => {
   return (
@@ -14,20 +14,22 @@ const MenuList: React.FC<Props> = ({ visibility }) => {
               className="block mx-auto px-8 md:px-4 py-4 text-gray-700 hover:text-opacity-70 hover:underline"
               style={{ maxWidth: "640px" }}
             >
-              ホーム
+              Home
             </a>
           </a>
         </li>
-        <li className="border-t md:border-t-0 border-gray-20 md:inline-block">
-          <a href="/collections/all">
-            <a
-              className="block mx-auto px-8 md:px-4 py-4 text-gray-700 hover:text-opacity-70 hover:underline"
-              style={{ maxWidth: "640px" }}
-            >
-              カタログ
+        {["all", "apple", "watch"].map((title) => (
+          <li className="border-t md:border-t-0 border-gray-20 md:inline-block">
+            <a href={`/collections/${title}`}>
+              <a
+                className="block mx-auto px-8 md:px-4 py-4 text-gray-700 hover:text-opacity-70 hover:underline"
+                style={{ maxWidth: "640px" }}
+              >
+                {title[0].toUpperCase() + title.slice(1)}
+              </a>
             </a>
-          </a>
-        </li>
+          </li>
+        ))}
       </ul>
     </nav>
   );
