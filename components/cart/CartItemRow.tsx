48cfa2da69d0a7e4f18c41b307f1fff3ea217fb7
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2021-07-17 15:22:27 +0900

    implement cart page style

--- components/cart/CartItemRow.tsx
+++ components/cart/CartItemRow.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import Image from "next/image";
+import { makeStyles } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { LineItem } from "shopify-buy";
+
+const useStyles = makeStyles({
+  myFlex: {
+    flex: "5rem 0 0",
+    paddingRight: "1.5rem",
+  },
+  myFlex2: {
+    flex: "9rem 0 0",
+    paddingRight: "3rem",
+  },
+});
 
 type Props = {
   item: any;
 };
 
 const CartItemRow: React.FC<Props> = ({ item }) => {
+  const classes = useStyles();
+  const matches = useMediaQuery("(min-width:768px)");
+
   const { handle, quantity, title } = item;
   const price = Number(item.variant.price);
   const subtotal = price * quantity;
@@ -15,12 +31,29 @@ const CartItemRow: React.FC<Props> = ({ item }) => {
   return (
     <tr className="border-gray-200 border-b">
       <td className="p-6 pl-0 text-left w-3/6">
-        <figure style={{ margin: 0 }}>
-          <a href={`/products/${handle}`}>
-            <Image priority src={imgSrc} height={150} width={150} />
-          </a>
-        </figure>
-        {title}
+        <div className="flex">
+          <figure className={matches ? classes.myFlex2 : classes.myFlex}>
+            <a href={`/products/${handle}`}>
+              <Image priority src={imgSrc} height={150} width={150} />
+            </a>
+          </figure>
+          <div>
+            <div className="font-semibold text-gray-700 text-base">{title}</div>
+            <div className="mt-3">
+              {item.variant.selectedOptions.map((opt, idx) => (
+                <div className="text-gray-600 text-sm" key={idx}>
+                  <span>{opt.name}:</span>
+                  <span>{opt.value}</span>
+                </div>
+              ))}
+            </div>
+            <div className="mt-3">
+              <button className="border-b border-gray-600 text-gray-600 text-sm">
+                削除
+              </button>
+            </div>
+          </div>
+        </div>
       </td>
       <td className="p-6 text-right w-1/6">
         <div>¥{price.toLocaleString("ja-JP")}</div>
