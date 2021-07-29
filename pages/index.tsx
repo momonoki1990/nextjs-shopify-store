c4c7bf4b35bda94b788da21ad1d6d11c20d52dfa
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2021-07-29 20:48:20 +0900

    remove unnecessary files and add messages to not implemented pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import Layout from "components/common/Layout"
+import React from "react";
+import Layout from "components/common/Layout";
 
 const home: React.FC = () => (
   <Layout>
-    <div>ただのトップページです。</div>
-    <a href="/collections/all">
-      カタログ(/collections/allへ)
-    </a>
+    <div className="text-center">This is top page and not implemented.</div>
   </Layout>
-)
+);
 
 export default home;
