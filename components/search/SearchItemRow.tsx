36483096c053d1673cf537ca4f0267e493c88400
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2021-08-05 06:58:32 +0900

    change flex css on SearchItemRow

--- components/search/SearchItemRow.tsx
+++ components/search/SearchItemRow.tsx
@@ -1,27 +1,12 @@
 import React from "react";
 import Image from "next/image";
-import { makeStyles } from "@material-ui/core/styles";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { Product } from "lib/graphql/product/getProductsByTitle";
-
-export const useStyles = makeStyles({
-  flexSm: {
-    flex: "0 0 4rem",
-    marginRight: "1rem",
-  },
-  flexMd: {
-    flex: "0 0 6rem",
-    marginRight: "1.5rem",
-  },
-});
 
 type Props = {
   product: Product;
 };
 
 export const SearchItemRow: React.FC<Props> = ({ product }) => {
-  const classes = useStyles();
-  const isOverMd = useMediaQuery("(min-width:768px)");
   const priceVaries = !(product.priceMax === product.priceMin);
 
   return (
@@ -30,15 +15,15 @@ export const SearchItemRow: React.FC<Props> = ({ product }) => {
         href={`/products/${product.handle}`}
         className="flex font-semibold items-center justify-center mb-5 text-sm md:text-base text-gray-700"
       >
-        <div className="flex items-center justify-center">
-          <figure className={isOverMd ? classes.flexMd : classes.flexSm}>
+        <div className="flex items-center justify-center w-full">
+          <figure className="flex-grow-0 flex-shrink-0 mr-4 md:mr-6 w-16 md:w-24">
             <Image
               src={product.images[0].originalSrc}
               height={150}
               width={150}
             />
           </figure>
-          <div>{product.title}</div>
+          <div className="w-full">{product.title}</div>
         </div>
         <div className="ml-4 md:ml-6 w-1/5 whitespace-nowrap">
           ¥{product.priceMin.toLocaleString("ja-JP")}
