5e2578001a05199f9c73288196d4bad8d956bdeb
zakiAzfar <61076339+zakiAzfar@users.noreply.github.com>
2021-08-19 15:24:57 +0530

    added slim progress bar on page change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,29 @@
 import "styles/global.css";
-import 'tailwindcss/tailwind.css'
+import "tailwindcss/tailwind.css";
 // Import Swiper styles
 import "swiper/swiper.min.css";
 import "swiper/components/navigation/navigation.min.css";
 
+import nProgress from "nprogress";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", nProgress.start);
+    router.events.on("routeChangeComplete", nProgress.done);
+    router.events.on("routeChangeError", nProgress.done);
+
+    return () => {
+      router.events.off("routeChangeStart", nProgress.start);
+      router.events.off("routeChangeComplete", nProgress.done);
+      router.events.off("routeChangeError", nProgress.done);
+    };
+  }, [router]);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
